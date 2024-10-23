aced23a82b3fde04539e28307aa94d45f8398483
André Sampaio <sam_ger@hotmail.com>
2024-10-23 16:16:09 -0300

    Adicionado aulas

--- aula54_exercise_with_lists.py
+++ aula54_exercise_with_lists.py
@@ -12,53 +12,44 @@ O programa não pode quebrar com erros
 de índices inexistetes. 
 
 '''
+import os
 
 shopping_list = list()
-
 while True:
-    
     option = input('\n[i]nsert [d]elete [l]ist [c]lose the program ---> ').lower()
-
     if option not in {'i', 'd', 'l', 'c'}:
         print('\nPlease choose only one of the four options!')
         continue
-    
     if option in 'i':
+        os.system('cls')
         item = input('\nWhite the desired product: ')
         shopping_list.append(item)
         continue
-
     if option in 'l':
-        print('\n Shopping list:\n')
-        for iterator in enumerate(shopping_list):
-            print(iterator)
-        continue
-
+        os.system('cls')
+        if len(shopping_list) == 0:
+            print('There is nothing to list')
+            continue
+        else:
+            print('\n Shopping list:\n')
+            for iterator in enumerate(shopping_list):
+                print(iterator)
+            continue
     if option in 'd':
-        
-        try:
-            error_value = True
-
-            while error_value:
-
-                index_number = input('\nEnter the number corresponding to the item: ')
-                index_number = int(index_number)
-                
-                if index_number <= len(shopping_list):
+        os.system('cls')
+        error_value = True
+        while error_value:
+            try:
+                while error_value:
+                    index_number = input('\nEnter the number corresponding to the item: ')
+                    index_number = int(index_number)
                     for index, value in enumerate(shopping_list):
-                        if index_number == index:
-                            del shopping_list[index_number]
-                            error_value = False
-                    
-                else:
-                    print('\nThe number entered does not match any product in the list.')
-             
-        except:
-            print('\nPlease enter the "number" corresponding to the desired item.')
-
-    
-        continue   
-
+                        del shopping_list[index_number]
+                        error_value = False
+            except ValueError:
+                print('\nPlease enter the "number" corresponding to the desired item.')
+            except IndexError:
+                print('\nThe number entered does not match any product in the list.')
     if option in 'c':
         print('\n')
         break
